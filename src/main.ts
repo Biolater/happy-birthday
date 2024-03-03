5e82b074561292ea4663446cb1a055c3cd906913
Biolater <yusifovmurad1@gmail.com>
2024-03-03 09:14:45 +0400

    commit 2

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
+const errorContainer = document.querySelector(".error") as HTMLDivElement;
 const startButton = document.getElementById('start') as HTMLButtonElement;
 const inputSection = document.getElementById('input-section') as HTMLInputElement;
 const nextButton = document.getElementById('next-button') as HTMLButtonElement;
 const nextButton2 = document.getElementById('next-button-2') as HTMLButtonElement;
+const nextButton3 = document.getElementById('next-button-3') as HTMLButtonElement;
 const datePicker = document.getElementById('date-pick') as HTMLDivElement;
 const dateInput = document.getElementById('date-input') as HTMLInputElement;
+const messageParent = document.getElementById('message-parent') as HTMLDivElement;
+const messageInput = document.getElementById('message-input') as HTMLInputElement;
 const onlyLettersRegex = /^[a-zA-Z]+$/;
 let enteredName: string;
+let enteredMessage: string;
+
 const dateRegex = /^(0[1-9]|1[0-2])\/(0[1-9]|[1-2][0-9]|3[0-1])\/\d{4}$/;
 startButton?.addEventListener('click', (e: any) => {
   e.target?.classList.add('fade-out');
@@ -32,8 +38,7 @@ inputSection?.addEventListener('input', (e: any) => {
 nextButton?.addEventListener('click', () => {
   const inputSectionText: string | null = (inputSection as HTMLInputElement)?.value.trim();
 
-  const errorContainer = document.querySelector(".error") as HTMLDivElement;
-  errorContainer.innerHTML = ''; 
+  errorContainer.innerHTML = '';
 
   if (inputSectionText && onlyLettersRegex.test(inputSectionText)) {
     inputSection.classList.remove("fade-in");
@@ -60,16 +65,42 @@ nextButton?.addEventListener('click', () => {
   }
 });
 
-
-
 nextButton2?.addEventListener('click', () => {
+  errorContainer.innerHTML = "";
   const dateInputText: string | null = (dateInput as HTMLInputElement)?.value.trim();
-  if(dateInputText && dateRegex.test(dateInputText)){
+  if (dateInputText && dateRegex.test(dateInputText)) {
     datePicker.classList.add("fade-out");
     nextButton2.classList.add("fade-out");
+    nextButton3?.classList.remove("hidden");
     setTimeout(() => {
       datePicker.classList.add("hidden");
       nextButton2.classList.add("hidden");
+      messageParent.classList.remove("hidden");
+      messageParent.classList.remove("fade-out");
+      messageParent.classList.add("flex")
+      messageInput.classList.remove("fade-out")
+      messageInput.classList.remove("hidden");
+      messageInput.classList.remove("fade-out");
+      messageParent.classList.add("fade-in");
     }, 400);
+  } else {
+    const pTag = document.createElement('p') as HTMLParagraphElement;
+    pTag.textContent = "Please enter a valid date in the format DD/MM /YYYY";
+    pTag.className = "absolute text-center top-[70px] text-color-500";
+    errorContainer.appendChild(pTag);
+
+  }
+})
+
+messageInput?.addEventListener('input', (e:any) => {
+  const input = e.target.value;
+  if (input.length > 0) {
+    setTimeout(() => {
+      nextButton3?.classList.remove("fade-out");
+      nextButton3?.classList.add("fade-in");
+    },200)
+  } else {
+    nextButton3?.classList.add("fade-out");
+    nextButton3?.classList.remove("fade-in");
   }
 })
